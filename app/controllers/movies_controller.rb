fac342749ddba48f765e56a80a2c5897baed416d
Chris Kahn <ckahn@users.noreply.github.com>
2015-07-24 16:15:15 -0700

    Add show page that uses API data

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,14 @@
+require 'open-uri'
+require 'json'
+
 class MoviesController < ApplicationController
   def index
+  end
+
+  def show
+    @film_data = JSON.load(
+      open('http://api.themoviedb.org/3/movie/' + params[:id] +
+      '?api_key=e505edada279c368d098a520d3fd7992')
+    )
   end
 end
