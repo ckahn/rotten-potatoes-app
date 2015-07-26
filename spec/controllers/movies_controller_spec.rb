389012739489560af25ee398b2e178e1fc4f7cb5
Chris Kahn <ckahn@users.noreply.github.com>
2015-07-25 18:34:57 -0700

    Add reviews controller tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
-  movie_id = 295099
+  MOVIE_ID = 295099
+  MOVIE_TITLE = "The Go-Go Boys: The Inside Story of Cannon Films"
 
   describe "GET #index" do
     it "returns http success" do
@@ -17,34 +18,25 @@ RSpec.describe MoviesController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show, id: movie_id
+      get :show, id: MOVIE_ID
       expect(response).to have_http_status(:success)
     end
 
     it "renders the show template" do
-      get :show, id: movie_id
+      get :show, id: MOVIE_ID
       expect(response).to render_template("show")
     end
 
-    it "gets the movie's data" do
-      get :show, id: movie_id
-      expect(assigns(:movie_data)["title"]).to eq("The Go-Go Boys: The Inside Story of Cannon Films")
+    it "gets the movie's API data" do
+      get :show, id: MOVIE_ID
+      expect(assigns(:movie_data)["title"]).to eq(MOVIE_TITLE)
     end
 
-    it "gets the movie's newer reviews first" do
-      review1 = Review.create!(
-        email: "user1@email.com",
-        movie_id: movie_id,
-        movie_title: "The Go-Go Boys: The Inside Story of Cannon Films",
-        rating: "1"
-      )
-      review2 = Review.create!(
-        email: "user2@email.com",
-        movie_id: movie_id,
-        movie_title: "The Go-Go Boys: The Inside Story of Cannon Films",
-        rating: "1"
-      )
-      get :show, id: 295099
+    it "gets the movie's reviews (newest first)" do
+      attrs = { movie_id: MOVIE_ID, movie_title: MOVIE_TITLE, rating: "1" }
+      review1 = Review.create!(attrs.merge(email: "user1@email.com"))
+      review2 = Review.create!(attrs.merge(email: "user2@email.com"))
+      get :show, id: MOVIE_ID
       expect(assigns(:reviews)).to eq([review2, review1])
     end
   end
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsController, type: :controller do
+  attrs = { movie_id: 1, movie_title: "Title", rating: "4" }
 
+  describe "POST #create" do
+    context "when params are valid" do
+      let(:valid_attrs) do
+        { email: "user@email.com",
+          movie_id: 1,
+          movie_title: "Title",
+          rating: "4" }
+      end
+
+      it "should save model" do
+        expect {
+          post :create, review: valid_attrs
+        }.to change(Review, :count).by(1)
+      end
+    end
+
+    context "when params are invalid" do
+      # TODO find out why an empty hash produces errors
+      let(:invalid_attrs) { { email: "" } }
+
+      it "should not save model" do
+        expect {
+          post :create, review: invalid_attrs
+        }.to_not change(Review, :count)
+      end
+
+      it "should return 422 error" do
+        post :create, review: invalid_attrs
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "gets all reviews (newest first)" do
+      review1 = Review.create!(attrs.merge(email: "user1@email.com"))
+      review2 = Review.create!(attrs.merge(email: "user2@email.com"))
+      get :index
+      expect(assigns(:reviews)).to eq([review2, review1])
+    end
+  end
 end
