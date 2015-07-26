3158b76523a6e8a3b3722250c46f8dce109ef1a2
Chris Kahn <ckahn@users.noreply.github.com>
2015-07-25 17:33:07 -0700

    Add movies controller tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,12 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
+  movie_id = 295099
 
-  describe "GET #reviews" do
+  describe "GET #index" do
     it "returns http success" do
-      get :reviews
+      get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
     end
   end
 
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, id: movie_id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the show template" do
+      get :show, id: movie_id
+      expect(response).to render_template("show")
+    end
+
+    it "gets the movie's data" do
+      get :show, id: movie_id
+      expect(assigns(:movie_data)["title"]).to eq("The Go-Go Boys: The Inside Story of Cannon Films")
+    end
+
+    it "gets the movie's newer reviews first" do
+      review1 = Review.create!(
+        email: "user1@email.com",
+        movie_id: movie_id,
+        movie_title: "The Go-Go Boys: The Inside Story of Cannon Films",
+        rating: "1"
+      )
+      review2 = Review.create!(
+        email: "user2@email.com",
+        movie_id: movie_id,
+        movie_title: "The Go-Go Boys: The Inside Story of Cannon Films",
+        rating: "1"
+      )
+      get :show, id: 295099
+      expect(assigns(:reviews)).to eq([review2, review1])
+    end
+  end
 end
